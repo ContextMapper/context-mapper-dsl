fa4ee083aa95bef72c803fcf44f2830511e41567
Stefan Kapferer <stefan@kapferer.ch>
2018-11-12 16:36:51 +0100

    Add release trigger script

---
+++ .travis/releaseTrigger.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+echo "This will trigger a release job on Travis. Are you sure you want to continue? (Y/n)"
+
+read CONTINUE_RELEASE
+
+if [ "$CONTINUE_RELEASE" = "Y" ]; then
+  TRAVIS_TOKEN=$(travis token --pro)
+  TRAVIS_REQUEST='{
+   "request": {
+   "message": "Trigger Release",
+   "branch":"master",
+   "config": {
+     "script": "xvfb-run ./.travis/prepareReleaseTravis.sh"
+    }
+  }}'
+
+  curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_TOKEN" \
+   -d "$TRAVIS_REQUEST" \
+   https://api.travis-ci.com/repo/stefan-ka%2Fcontext-mapping-dsl/requests
+else
+  echo "Aborted."
+fi
