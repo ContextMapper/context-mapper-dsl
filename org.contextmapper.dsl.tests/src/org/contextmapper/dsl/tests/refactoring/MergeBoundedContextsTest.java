c35b85f52ababc74fea3c99aab2f5dce314f2157
Stefan Kapferer <stefan@kapferer.ch>
2019-05-18 17:50:50 +0200

    Fix wrong junit import

--- org.contextmapper.dsl.tests/src/org/contextmapper/dsl/tests/refactoring/MergeBoundedContextsTest.java
+++ org.contextmapper.dsl.tests/src/org/contextmapper/dsl/tests/refactoring/MergeBoundedContextsTest.java
@@ -1,6 +1,6 @@
 package org.contextmapper.dsl.tests.refactoring;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
