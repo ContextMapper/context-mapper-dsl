2fbcf20b0a512aa4efe5390978337fe78f67ee38
Stefan Kapferer <stefan@kapferer.ch>
2019-09-27 18:18:10 +0200

    Remove eclipse dependencies from ServiceCutterOutputModelFactory (#89)

--- org.contextmapper.dsl/src/org/contextmapper/dsl/generator/servicecutter/output/factory/ServiceCutterOutputModelFactory.java
+++ org.contextmapper.dsl/src/org/contextmapper/dsl/generator/servicecutter/output/factory/ServiceCutterOutputModelFactory.java
@@ -18,11 +18,6 @@ package org.contextmapper.dsl.generator.servicecutter.output.factory;
 import java.io.File;
 
 import org.contextmapper.dsl.generator.servicecutter.output.model.ServiceCutterOutputModel;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.emf.common.CommonPlugin;
-import org.eclipse.emf.common.util.URI;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -32,16 +27,6 @@ public class ServiceCutterOutputModelFactory {
 
 	public ServiceCutterOutputModelFactory() {
 		this.objectMapper = new ObjectMapper();
-	}
-
-	public ServiceCutterOutputModel createFromJsonFile(URI jsonFileURI) {
-		try {
-			URI resolvedFile = CommonPlugin.resolve(jsonFileURI);
-			IFile jsonFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(resolvedFile.toFileString()));
-			return createFromJsonFile(jsonFile.getFullPath().toFile());
-		} catch (Exception e) {
-			throw new ServiceCutterOutputModelReadingException(jsonFileURI.toPlatformString(true));
-		}
 	}
 
 	public ServiceCutterOutputModel createFromJsonFile(File jsonFile) {
