9df17b528fe3bb9c40bf1a7eec8da5d5580e1f0d
Stefan Kapferer <stefan@kapferer.ch>
2019-10-03 22:12:54 +0200

    #91: create class diagrams for subdomains in PlantUML generator (#99)

--- org.contextmapper.dsl/src/org/contextmapper/dsl/generator/PlantUMLGenerator.java
+++ org.contextmapper.dsl/src/org/contextmapper/dsl/generator/PlantUMLGenerator.java
@@ -17,8 +17,10 @@ package org.contextmapper.dsl.generator;
 
 import org.contextmapper.dsl.contextMappingDSL.BoundedContext;
 import org.contextmapper.dsl.contextMappingDSL.ContextMap;
-import org.contextmapper.dsl.generator.plantuml.PlantUMLClassDiagramCreator;
+import org.contextmapper.dsl.contextMappingDSL.Domain;
+import org.contextmapper.dsl.generator.plantuml.PlantUMLBoundedContextClassDiagramCreator;
 import org.contextmapper.dsl.generator.plantuml.PlantUMLComponentDiagramCreator;
+import org.contextmapper.dsl.generator.plantuml.PlantUMLSubdomainClassDiagramCreator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 
@@ -31,7 +33,14 @@ public class PlantUMLGenerator extends AbstractContextMapGenerator {
 		String fileName = inputFileURI.trimFileExtension().lastSegment();
 		fsa.generateFile(fileName + "_ContextMap." + PLANT_UML_FILE_EXT, new PlantUMLComponentDiagramCreator().createDiagram(contextMap));
 		for (BoundedContext boundedContext : contextMap.getBoundedContexts()) {
-			fsa.generateFile(fileName + "_BC_" + boundedContext.getName() + "." + PLANT_UML_FILE_EXT, new PlantUMLClassDiagramCreator().createDiagram(boundedContext));
+			fsa.generateFile(fileName + "_BC_" + boundedContext.getName() + "." + PLANT_UML_FILE_EXT,
+					new PlantUMLBoundedContextClassDiagramCreator().createDiagram(boundedContext));
+		}
+		for (Domain domain : this.contextMappingModel.getDomains()) {
+			domain.getSubdomains().stream().filter(subdomain -> subdomain.getEntities().size() > 0).forEach(subdomain -> {
+				fsa.generateFile(fileName + "_SD_" + subdomain.getName() + "." + PLANT_UML_FILE_EXT,
+						new PlantUMLSubdomainClassDiagramCreator(domain.getName()).createDiagram(subdomain));
+			});
 		}
 	}
 
---
+++ org.contextmapper.dsl/src/org/contextmapper/dsl/generator/plantuml/PlantUMLBoundedContextClassDiagramCreator.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 The Context Mapper Project Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.contextmapper.dsl.generator.plantuml;
+
+import java.util.List;
+
+import org.contextmapper.dsl.contextMappingDSL.Aggregate;
+import org.contextmapper.dsl.contextMappingDSL.BoundedContext;
+import org.contextmapper.dsl.contextMappingDSL.Module;
+import org.contextmapper.dsl.contextMappingDSL.Subdomain;
+import org.contextmapper.dsl.validation.ValidationMessages;
+import org.contextmapper.tactic.dsl.tacticdsl.Entity;
+import org.contextmapper.tactic.dsl.tacticdsl.Service;
+import org.contextmapper.tactic.dsl.tacticdsl.ServiceOperation;
+import org.contextmapper.tactic.dsl.tacticdsl.SimpleDomainObject;
+import org.eclipse.xtext.EcoreUtil2;
+
+import com.google.common.collect.Lists;
+
+public class PlantUMLBoundedContextClassDiagramCreator extends AbstractPlantUMLClassDiagramCreator<BoundedContext> implements PlantUMLDiagramCreator<BoundedContext> {
+
+	@Override
+	protected void printDiagramContent(BoundedContext boundedContext) {
+		this.relationships = Lists.newArrayList();
+		this.domainObjects = EcoreUtil2.<SimpleDomainObject>getAllContentsOfType(boundedContext, SimpleDomainObject.class);
+		if (this.domainObjects.size() <= 0) {
+			printEmptyDiagramNote();
+			return;
+		}
+		for (Module module : boundedContext.getModules()) {
+			printModule(module);
+		}
+		for (Aggregate aggregate : boundedContext.getAggregates()) {
+			printAggregate(aggregate, 0);
+		}
+		printReferences(0);
+		printSubdomainLegend(boundedContext.getImplementedSubdomains());
+	}
+
+	private void printSubdomainLegend(List<Subdomain> subdomains) {
+		if (subdomains.isEmpty())
+			return;
+		sb.append("legend left");
+		linebreak();
+		for (Subdomain subdomain : subdomains) {
+			if (subdomain.getEntities().isEmpty()) {
+				sb.append("  ").append("This bounded context implements the subdomain '" + subdomain.getName() + "'.");
+			} else {
+				sb.append("  ").append("This bounded context implements the subdomain '" + subdomain.getName() + "', which contains the following entities:");
+			}
+			linebreak();
+			for (Entity entity : subdomain.getEntities()) {
+				sb.append("  ").append(" - ").append(entity.getName());
+				linebreak();
+			}
+		}
+		sb.append("end legend");
+		linebreak();
+	}
+
+	private void printEmptyDiagramNote() {
+		sb.append("note").append(" ").append("\"").append(ValidationMessages.EMPTY_UML_CLASS_DIAGRAM_MESSAGE).append("\"").append(" as EmptyDiagramError");
+		linebreak();
+	}
+
+	private void printModule(Module module) {
+		sb.append("package ");
+		if (module.getBasePackage() != null && !"".equals(module.getBasePackage()))
+			sb.append(module.getBasePackage()).append(".").append(module.getName());
+		else
+			sb.append(module.getName());
+		sb.append(" {");
+		linebreak();
+		for (Aggregate aggregate : module.getAggregates()) {
+			printAggregate(aggregate, 1);
+		}
+		for (SimpleDomainObject simpleDomainObject : module.getDomainObjects()) {
+			printDomainObject(simpleDomainObject, 1);
+		}
+		for (Service service : module.getServices()) {
+			printService(service, 1);
+		}
+		sb.append("}");
+		linebreak();
+	}
+
+	private void printAggregate(Aggregate aggregate, int indentation) {
+		printIndentation(indentation);
+		sb.append("package ").append("\"'").append(aggregate.getName()).append("' ").append("Aggregate\"").append(" <<Rectangle>> ").append("{");
+		linebreak();
+		for (SimpleDomainObject domainObject : aggregate.getDomainObjects()) {
+			printDomainObject(domainObject, indentation + 1);
+		}
+		for (Service service : aggregate.getServices()) {
+			printService(service, indentation + 1);
+		}
+		printIndentation(indentation);
+		sb.append("}");
+		linebreak();
+	}
+
+	private void printService(Service service, int indentation) {
+		printIndentation(indentation);
+		sb.append("class").append(" ").append(service.getName());
+		sb.append(" <<Service>> ");
+		sb.append("{");
+		linebreak();
+		printServiceOperations(service.getName(), service.getOperations(), indentation + 1);
+		printIndentation(indentation);
+		sb.append("}");
+		linebreak();
+	}
+
+	private void printServiceOperations(String objectName, List<ServiceOperation> operations, int indentation) {
+		for (ServiceOperation operation : operations) {
+			printOperation(objectName, operation.getName(), operation.getReturnType(), operation.getParameters(), indentation);
+		}
+	}
+	
+}
