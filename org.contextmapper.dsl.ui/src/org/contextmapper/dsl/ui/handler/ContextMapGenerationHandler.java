56a6d2006b8ec87a18caa05e75efe016d4e04299
Stefan Kapferer <stefan@kapferer.ch>
2019-11-13 13:22:36 +0100

    Graphviz installation check only 'soft-try' (continue of PATH cannot be checked)

--- org.contextmapper.dsl.ui/src/org/contextmapper/dsl/ui/handler/ContextMapGenerationHandler.java
+++ org.contextmapper.dsl.ui/src/org/contextmapper/dsl/ui/handler/ContextMapGenerationHandler.java
@@ -19,12 +19,16 @@ import org.contextmapper.dsl.generator.ContextMapGenerator;
 import org.contextmapper.dsl.generator.exception.GeneratorInputException;
 import org.contextmapper.dsl.ui.handler.wizard.GenerateContextMapContext;
 import org.contextmapper.dsl.ui.handler.wizard.GenerateContextMapWizard;
+import org.contextmapper.dsl.ui.internal.DslActivator;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.xtext.generator.GeneratorContext;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGenerator2;
@@ -43,10 +47,18 @@ public class ContextMapGenerationHandler extends AbstractGenerationHandler {
 
 	@Override
 	protected void runGeneration(Resource resource, ExecutionEvent event, IFileSystemAccess2 fsa) {
-		if (!generator.isGraphvizInstalled()) {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event), "Graphviz installation not found",
-					"Graphviz has not been found on your system. Ensure it is installed and the binaries are part of your PATH environment variable.");
-			return;
+		try {
+			if (!generator.isGraphvizInstalled()) {
+				MessageDialog.openInformation(HandlerUtil.getActiveShell(event), "Graphviz installation not found",
+						"Graphviz has not been found on your system. Ensure it is installed and the binaries are part of your PATH environment variable.");
+				return;
+			}
+		} catch (Exception e) {
+			String message = e.getMessage() != null && !"".equals(e.getMessage()) ? e.getMessage() : e.getClass().getName() + " occurred in " + this.getClass().getName();
+			Status status = new Status(IStatus.ERROR, DslActivator.PLUGIN_ID, message, e);
+			StatusManager.getManager().handle(status);
+			MessageDialog.openInformation(HandlerUtil.getActiveShell(event), "Graphviz installation check",
+					"Your PATH variable could not be parsed to check if Graphviz is installed. The generator may not work if Graphviz is not available.");
 		}
 
 		GenerateContextMapContext context = new GenerateContextMapContext();
