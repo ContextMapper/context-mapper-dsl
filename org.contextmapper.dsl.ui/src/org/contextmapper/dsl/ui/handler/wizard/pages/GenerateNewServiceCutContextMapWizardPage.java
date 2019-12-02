e62867b889fbcb25422f1c087230f681d04bc422
Stefan Kapferer <stefan@kapferer.ch>
2019-12-02 14:56:08 +0100

    #156 Service Cut generator UI: improve dialog to prioritize a specific criteria group / new criteria groups (#169)

--- org.contextmapper.dsl.ui/src/org/contextmapper/dsl/ui/handler/wizard/pages/GenerateNewServiceCutContextMapWizardPage.java
+++ org.contextmapper.dsl.ui/src/org/contextmapper/dsl/ui/handler/wizard/pages/GenerateNewServiceCutContextMapWizardPage.java
@@ -31,7 +31,9 @@ import static ch.hsr.servicecutter.model.criteria.CouplingCriterion.STORAGE_SIMI
 import static ch.hsr.servicecutter.model.criteria.CouplingCriterion.STRUCTURAL_VOLATILITY;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.contextmapper.dsl.ui.handler.wizard.pages.components.SCLFileChooser;
@@ -45,6 +47,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
@@ -60,6 +63,7 @@ public class GenerateNewServiceCutContextMapWizardPage extends ContextMapperWiza
 	private SolverConfiguration solverConfiguration;
 	private SCLFileChooser sclFileChooser;
 	private IFile initialSCLFile;
+	private Map<String, Combo> allPriorityCombos = new HashMap<>();
 
 	private Combo algorithmSelectionCombo;
 
@@ -83,23 +87,23 @@ public class GenerateNewServiceCutContextMapWizardPage extends ContextMapperWiza
 	public void createControl(Composite parent) {
 		Composite mainComposite = new Composite(parent, SWT.NONE);
 		mainComposite.setLayout(GridLayoutFactory.fillDefaults().create());
-		
+
 		ScrolledComposite scrollComp = new ScrolledComposite(mainComposite, SWT.V_SCROLL);
 		scrollComp.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).hint(SWT.DEFAULT, 500).create());
-		scrollComp.setLayout(new GridLayout(1,false));
+		scrollComp.setLayout(new GridLayout(1, false));
 		scrollComp.setExpandHorizontal(true);
 		scrollComp.setExpandVertical(true);
 		scrollComp.setAlwaysShowScrollBars(true);
-		
+
 		container = new Composite(scrollComp, SWT.NONE);
 		scrollComp.setContent(container);
-		
+
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
 		layout.verticalSpacing = 10;
 		container.setLayout(layout);
 		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
+
 		// SCL file selection
 		new Label(container, SWT.NONE).setText("User representations:");
 		sclFileChooser = new SCLFileChooser(container);
@@ -122,60 +126,144 @@ public class GenerateNewServiceCutContextMapWizardPage extends ContextMapperWiza
 			}
 		});
 
-		// Cohesiveness Criteria
-		Group cohesivenessCriteriaGroup = new Group(container, SWT.NONE);
-		GridLayout cohesivenessGridLayout = new GridLayout();
-		cohesivenessGridLayout.numColumns = 2;
-		cohesivenessGridLayout.makeColumnsEqualWidth = true;
-		cohesivenessCriteriaGroup.setLayout(cohesivenessGridLayout);
-		GridData cohesivenessCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
-		cohesivenessCriteriaGroupGridData.horizontalSpan = 2;
-		cohesivenessCriteriaGroup.setLayoutData(cohesivenessCriteriaGroupGridData);
-		cohesivenessCriteriaGroup.setText("Cohesiveness Criteria");
+		// Functional/Domain Model Criteria
+		Group functionalCriteriaGroup = new Group(container, SWT.NONE);
+		GridLayout functionalGridLayout = new GridLayout();
+		functionalGridLayout.numColumns = 2;
+		functionalGridLayout.makeColumnsEqualWidth = true;
+		functionalCriteriaGroup.setLayout(functionalGridLayout);
+		GridData functionalCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
+		functionalCriteriaGroupGridData.horizontalSpan = 2;
+		functionalCriteriaGroup.setLayoutData(functionalCriteriaGroupGridData);
+		functionalCriteriaGroup.setText("Functional / Domain Model - Criteria Priorities");
 
-		createCriteriaPrioritySelectionCombo(cohesivenessCriteriaGroup, IDENTITY_LIFECYCLE);
-		createCriteriaPrioritySelectionCombo(cohesivenessCriteriaGroup, SEMANTIC_PROXIMITY);
-		createCriteriaPrioritySelectionCombo(cohesivenessCriteriaGroup, SHARED_OWNER);
-		createCriteriaPrioritySelectionCombo(cohesivenessCriteriaGroup, LATENCY);
-		createCriteriaPrioritySelectionCombo(cohesivenessCriteriaGroup, SECURITY_CONTEXUALITY);
+		createCriteriaPrioritySelectionCombo(functionalCriteriaGroup, IDENTITY_LIFECYCLE);
+		createCriteriaPrioritySelectionCombo(functionalCriteriaGroup, SEMANTIC_PROXIMITY);
+		createCriteriaPrioritySelectionCombo(functionalCriteriaGroup, STRUCTURAL_VOLATILITY);
+		createCriteriaPrioritySelectionCombo(functionalCriteriaGroup, CONTENT_VOLATILITY);
 
-		// Compatibility Criteria
-		Group compatibilityCriteriaGroup = new Group(container, SWT.NONE);
-		GridLayout compatibilityGridLayout = new GridLayout();
-		compatibilityGridLayout.numColumns = 2;
-		compatibilityGridLayout.makeColumnsEqualWidth = true;
-		compatibilityCriteriaGroup.setLayout(compatibilityGridLayout);
-		GridData compatibilityCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
-		compatibilityCriteriaGroupGridData.horizontalSpan = 2;
-		compatibilityCriteriaGroup.setLayoutData(compatibilityCriteriaGroupGridData);
-		compatibilityCriteriaGroup.setText("Compatibility Criteria");
+		new Label(functionalCriteriaGroup, SWT.NONE);
+		Button prioritizeFunctionalGroupButton = new Button(functionalCriteriaGroup, SWT.NONE);
+		prioritizeFunctionalGroupButton.setText("Prioritize criteria of this group");
+		prioritizeFunctionalGroupButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		prioritizeFunctionalGroupButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				for (Map.Entry<String, Combo> priorityComboEntry : allPriorityCombos.entrySet()) {
+					changePriority(priorityComboEntry.getKey(), SolverPriority.S);
+				}
+				changePriority(IDENTITY_LIFECYCLE, SolverPriority.XL);
+				changePriority(SEMANTIC_PROXIMITY, SolverPriority.XL);
+				changePriority(STRUCTURAL_VOLATILITY, SolverPriority.XL);
+				changePriority(CONTENT_VOLATILITY, SolverPriority.XL);
+			}
+		});
 
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, STRUCTURAL_VOLATILITY);
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, CONSISTENCY);
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, AVAILABILITY);
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, CONTENT_VOLATILITY);
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, STORAGE_SIMILARITY);
-		createCriteriaPrioritySelectionCombo(compatibilityCriteriaGroup, SECURITY_CRITICALITY);
+		// Runtime Quality Criteria
+		Group runtimeQualityCriteriaGroup = new Group(container, SWT.NONE);
+		GridLayout runtimeQualityGridLayout = new GridLayout();
+		runtimeQualityGridLayout.numColumns = 2;
+		runtimeQualityGridLayout.makeColumnsEqualWidth = true;
+		runtimeQualityCriteriaGroup.setLayout(runtimeQualityGridLayout);
+		GridData runtimeQualityCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
+		runtimeQualityCriteriaGroupGridData.horizontalSpan = 2;
+		runtimeQualityCriteriaGroup.setLayoutData(runtimeQualityCriteriaGroupGridData);
+		runtimeQualityCriteriaGroup.setText("Runtime Quality - Criteria Priorities");
 
-		// Constraints Criteria
-		Group constraintsCriteriaGroup = new Group(container, SWT.NONE);
-		GridLayout constraintsGridLayout = new GridLayout();
-		constraintsGridLayout.numColumns = 2;
-		constraintsGridLayout.makeColumnsEqualWidth = true;
-		constraintsCriteriaGroup.setLayout(constraintsGridLayout);
-		GridData constraintsCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
-		constraintsCriteriaGroupGridData.horizontalSpan = 2;
-		constraintsCriteriaGroup.setLayoutData(constraintsCriteriaGroupGridData);
-		constraintsCriteriaGroup.setText("Constraints Criteria");
+		createCriteriaPrioritySelectionCombo(runtimeQualityCriteriaGroup, LATENCY);
+		createCriteriaPrioritySelectionCombo(runtimeQualityCriteriaGroup, AVAILABILITY);
+		createCriteriaPrioritySelectionCombo(runtimeQualityCriteriaGroup, SECURITY_CONTEXUALITY);
+		createCriteriaPrioritySelectionCombo(runtimeQualityCriteriaGroup, SECURITY_CRITICALITY);
+		createCriteriaPrioritySelectionCombo(runtimeQualityCriteriaGroup, SECURITY_CONSTRAINT);
 
-		createCriteriaPrioritySelectionCombo(constraintsCriteriaGroup, CONSISTENCY_CONSTRAINT);
-		createCriteriaPrioritySelectionCombo(constraintsCriteriaGroup, PREDEFINED_SERVICE);
-		createCriteriaPrioritySelectionCombo(constraintsCriteriaGroup, SECURITY_CONSTRAINT);
+		new Label(runtimeQualityCriteriaGroup, SWT.NONE);
+		Button prioritizeRuntimeQualityGroupButton = new Button(runtimeQualityCriteriaGroup, SWT.NONE);
+		prioritizeRuntimeQualityGroupButton.setText("Prioritize criteria of this group");
+		prioritizeRuntimeQualityGroupButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		prioritizeRuntimeQualityGroupButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				for (Map.Entry<String, Combo> priorityComboEntry : allPriorityCombos.entrySet()) {
+					changePriority(priorityComboEntry.getKey(), SolverPriority.S);
+				}
+				changePriority(LATENCY, SolverPriority.XL);
+				changePriority(AVAILABILITY, SolverPriority.XL);
+				changePriority(SECURITY_CONTEXUALITY, SolverPriority.XL);
+				changePriority(SECURITY_CRITICALITY, SolverPriority.XL);
+				changePriority(SECURITY_CONSTRAINT, SolverPriority.XL);
+			}
+		});
+		
+		// Data Criteria
+		Group dataCriteriaGroup = new Group(container, SWT.NONE);
+		GridLayout dataGridLayout = new GridLayout();
+		dataGridLayout.numColumns = 2;
+		dataGridLayout.makeColumnsEqualWidth = true;
+		dataCriteriaGroup.setLayout(dataGridLayout);
+		GridData dataCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
+		dataCriteriaGroupGridData.horizontalSpan = 2;
+		dataCriteriaGroup.setLayoutData(dataCriteriaGroupGridData);
+		dataCriteriaGroup.setText("Data - Criteria Priorities");
 
-		container.setLayout(new GridLayout(1,false));
+		createCriteriaPrioritySelectionCombo(dataCriteriaGroup, CONSISTENCY);
+		createCriteriaPrioritySelectionCombo(dataCriteriaGroup, CONSISTENCY_CONSTRAINT);
+		createCriteriaPrioritySelectionCombo(dataCriteriaGroup, STORAGE_SIMILARITY);
+		
+		new Label(dataCriteriaGroup, SWT.NONE);
+		Button prioritizeDataGroupButton = new Button(dataCriteriaGroup, SWT.NONE);
+		prioritizeDataGroupButton.setText("Prioritize criteria of this group");
+		prioritizeDataGroupButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		prioritizeDataGroupButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				for (Map.Entry<String, Combo> priorityComboEntry : allPriorityCombos.entrySet()) {
+					changePriority(priorityComboEntry.getKey(), SolverPriority.S);
+				}
+				changePriority(CONSISTENCY, SolverPriority.XL);
+				changePriority(CONSISTENCY_CONSTRAINT, SolverPriority.XL);
+				changePriority(STORAGE_SIMILARITY, SolverPriority.XL);
+			}
+		});
+
+		// Organizational Criteria
+		Group organizationalCriteriaGroup = new Group(container, SWT.NONE);
+		GridLayout organizationalGridLayout = new GridLayout();
+		organizationalGridLayout.numColumns = 2;
+		organizationalGridLayout.makeColumnsEqualWidth = true;
+		organizationalCriteriaGroup.setLayout(organizationalGridLayout);
+		GridData organizationalCriteriaGroupGridData = new GridData(GridData.FILL_HORIZONTAL);
+		organizationalCriteriaGroupGridData.horizontalSpan = 2;
+		organizationalCriteriaGroup.setLayoutData(organizationalCriteriaGroupGridData);
+		organizationalCriteriaGroup.setText("Organizational - Criteria Priorities");
+
+		createCriteriaPrioritySelectionCombo(organizationalCriteriaGroup, SHARED_OWNER);
+		createCriteriaPrioritySelectionCombo(organizationalCriteriaGroup, PREDEFINED_SERVICE);
+		
+		new Label(organizationalCriteriaGroup, SWT.NONE);
+		Button prioritizeOrganizationalGroupButton = new Button(organizationalCriteriaGroup, SWT.NONE);
+		prioritizeOrganizationalGroupButton.setText("Prioritize criteria of this group");
+		prioritizeOrganizationalGroupButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		prioritizeOrganizationalGroupButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				for (Map.Entry<String, Combo> priorityComboEntry : allPriorityCombos.entrySet()) {
+					changePriority(priorityComboEntry.getKey(), SolverPriority.S);
+				}
+				changePriority(SHARED_OWNER, SolverPriority.XL);
+				changePriority(PREDEFINED_SERVICE, SolverPriority.XL);
+			}
+		});
+
+		Button restoreDefaultPrioritiesButton = new Button(container, SWT.NONE);
+		restoreDefaultPrioritiesButton.setText("Restore Default Priorities");
+		restoreDefaultPrioritiesButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				for (Map.Entry<String, Combo> priorityComboEntry : allPriorityCombos.entrySet()) {
+					changePriority(priorityComboEntry.getKey(), SolverPriority.M);
+				}
+			}
+		});
+
+		container.setLayout(new GridLayout(1, false));
 		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		scrollComp.setMinSize(container.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-		
+
 		setControl(mainComposite);
 		setPageComplete(false);
 	}
@@ -194,6 +282,14 @@ public class GenerateNewServiceCutContextMapWizardPage extends ContextMapperWiza
 				setPageComplete(isPageComplete());
 			}
 		});
+		allPriorityCombos.put(criterionId, priorityCombo);
+	}
+
+	private void changePriority(String criterionId, SolverPriority priority) {
+		Combo priorityCombo = allPriorityCombos.get(criterionId);
+		priorityCombo.select(priorityCombo.indexOf(priority.name()));
+		solverConfiguration.setPriority(criterionId, SolverPriority.valueOf(priorityCombo.getText()));
+		setPageComplete(isPageComplete());
 	}
 
 	@Override
