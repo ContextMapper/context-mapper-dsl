df83877ea2675473b894b5666797990ed8a32913
Stefan Kapferer <stefan@kapferer.ch>
2020-07-27 10:26:51 +0200

    SCL example generator no longer needs a Context Map (#236)

--- org.contextmapper.dsl/src/org/contextmapper/dsl/generator/ServiceCutterUserRepresentationsExampleGenerator.java
+++ org.contextmapper.dsl/src/org/contextmapper/dsl/generator/ServiceCutterUserRepresentationsExampleGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 The Context Mapper Project Team
+ * Copyright 2018-2020 The Context Mapper Project Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ package org.contextmapper.dsl.generator;
 
 import java.io.IOException;
 
-import org.contextmapper.dsl.contextMappingDSL.ContextMap;
+import org.contextmapper.dsl.contextMappingDSL.ContextMappingModel;
 import org.contextmapper.dsl.generator.servicecutter.input.userrepresentations.UserRepresentationsExampleFactory;
 import org.contextmapper.servicecutter.dsl.ServiceCutterConfigurationDSLStandaloneSetup;
 import org.contextmapper.servicecutter.dsl.serviceCutterConfigurationDSL.ServiceCutterUserRepresentationsModel;
@@ -30,29 +30,29 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 
 import com.google.inject.Injector;
 
-public class ServiceCutterUserRepresentationsExampleGenerator extends AbstractContextMapGenerator {
+public class ServiceCutterUserRepresentationsExampleGenerator extends AbstractContextMappingModelGenerator {
 
 	@Override
-	protected void generateFromContextMap(ContextMap contextmap, IFileSystemAccess2 fsa, URI inputFileURI) {
+	protected void generateFromContextMappingModel(ContextMappingModel model, IFileSystemAccess2 fsa, URI inputFileURI) {
 		Injector injector = new ServiceCutterConfigurationDSLStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		EcoreUtil.resolveAll(contextMappingModel);
 		
 		UserRepresentationsExampleFactory factory = new UserRepresentationsExampleFactory();
-		ServiceCutterUserRepresentationsModel model = factory.createExampleModel(contextMappingModel);
+		ServiceCutterUserRepresentationsModel sclModel = factory.createExampleModel(contextMappingModel);
 
-		EcoreUtil.resolveAll(model);
+		EcoreUtil.resolveAll(sclModel);
 		URI inputURI = inputFileURI.trimFileExtension();
 		String fileName = inputURI.lastSegment();
 		URI outputURI = inputURI.trimSegments(1).appendSegment(fileName + "_user-representations").appendFileExtension("scl");
 		Resource sclResource = resourceSet.createResource(outputURI);
-		sclResource.getContents().add(model);
+		sclResource.getContents().add(sclModel);
 		try {
 			sclResource.save(null);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}		
 	}
 
 }
