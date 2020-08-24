2bc7895e0c17dffa7125941d6b27b1a165109658
Stefan Kapferer <stefan@kapferer.ch>
2020-08-24 15:07:47 +0200

    Initial version of 'Extract Suggested Service' refactoring

--- org.contextmapper.dsl.tests/src/org/contextmapper/dsl/refactoring/ExtractSuggestedServiceTest.java
+++ org.contextmapper.dsl.tests/src/org/contextmapper/dsl/refactoring/ExtractSuggestedServiceTest.java
@@ -17,14 +17,18 @@ package org.contextmapper.dsl.refactoring;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
 
 import org.contextmapper.dsl.cml.CMLResourceContainer;
+import org.contextmapper.dsl.contextMappingDSL.Aggregate;
 import org.contextmapper.dsl.contextMappingDSL.BoundedContext;
 import org.contextmapper.dsl.contextMappingDSL.ContextMappingDSLFactory;
 import org.contextmapper.dsl.exception.ContextMapperApplicationException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ExtractSuggestedServiceTest extends AbstractRefactoringTest {
 
@@ -43,6 +47,39 @@ public class ExtractSuggestedServiceTest extends AbstractRefactoringTest {
 
 		// then
 		assertEquals(2, model.getContextMappingModel().getBoundedContexts().size());
+		BoundedContext newBC = model.getContextMappingModel().getBoundedContexts().stream().filter(bc -> bc.getName().equals("ExtractedBC")).findFirst().get();
+		assertNotNull(newBC);
+		assertEquals(1, newBC.getAggregates().size());
+		Aggregate aggregate = newBC.getAggregates().get(0);
+		assertNotNull(aggregate);
+		assertEquals(10, aggregate.getDomainObjects().size());
+	}
+
+	@Test
+	public void canThrowExceptionInCaseThereAreNoAttributes() throws IOException {
+		// given
+		CMLResourceContainer model = getResourceCopyOfTestCML("extract-suggested-service-empty-model-test.cml");
+		BoundedContext contextToExtract = getResourceCopyOfTestCML("extract-suggested-service-DDD-sample_Markov_Clustering_Cut_1.cml").getContextMappingModel().getBoundedContexts().stream()
+				.filter(bc -> bc.getName().equals("Service_P")).findFirst().get();
+
+		// when, then
+		assertThrows(ContextMapperApplicationException.class, () -> {
+			new ExtractSuggestedService(contextToExtract, "Service_P").refactor(model);
+		});
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "extract-suggested-service-unsupported-type-test-1", "extract-suggested-service-unsupported-type-test-2" })
+	public void canThrowException4UnsupportedType(String baseTestFileName) throws IOException {
+		// given
+		CMLResourceContainer model = getResourceCopyOfTestCML(baseTestFileName + ".cml");
+		BoundedContext contextToExtract = getResourceCopyOfTestCML(baseTestFileName + "_Markov_Clustering_Cut_1.cml").getContextMappingModel().getBoundedContexts().stream()
+				.filter(bc -> bc.getName().equals("Service_A")).findFirst().get();
+
+		// when, then
+		assertThrows(ContextMapperApplicationException.class, () -> {
+			new ExtractSuggestedService(contextToExtract, "Service_A").refactor(model);
+		});
 	}
 
 	@Test
--- org.contextmapper.dsl/src/org/contextmapper/dsl/refactoring/ExtractSuggestedService.java
+++ org.contextmapper.dsl/src/org/contextmapper/dsl/refactoring/ExtractSuggestedService.java
@@ -16,17 +16,28 @@
 package org.contextmapper.dsl.refactoring;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.contextmapper.dsl.cml.CMLResourceContainer;
+import org.contextmapper.dsl.contextMappingDSL.Aggregate;
 import org.contextmapper.dsl.contextMappingDSL.BoundedContext;
 import org.contextmapper.dsl.contextMappingDSL.ContextMappingDSLFactory;
-import org.contextmapper.dsl.contextMappingDSL.ContextMappingModel;
+import org.contextmapper.dsl.contextMappingDSL.SculptorModule;
 import org.contextmapper.dsl.exception.ContextMapperApplicationException;
+import org.contextmapper.tactic.dsl.tacticdsl.Attribute;
+import org.contextmapper.tactic.dsl.tacticdsl.DomainEvent;
+import org.contextmapper.tactic.dsl.tacticdsl.DomainObject;
+import org.contextmapper.tactic.dsl.tacticdsl.Reference;
+import org.contextmapper.tactic.dsl.tacticdsl.TacticdslFactory;
+import org.contextmapper.tactic.dsl.tacticdsl.ValueObject;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.EcoreUtil2;
+
+import com.google.common.collect.Maps;
 
 import ch.hsr.servicecutter.solver.SolverAlgorithm;
 
@@ -43,20 +54,128 @@ public class ExtractSuggestedService extends AbstractRefactoring implements Sema
 	private BoundedContext suggestedService;
 	private String newBoundedContextName;
 
+	private Map<String, DomainObject> domainObjectMap;
+
 	public ExtractSuggestedService(BoundedContext suggestedService, String newBoundedContextName) {
 		this.suggestedService = suggestedService;
 		checkPreconditions();
 		this.newBoundedContextName = newBoundedContextName;
+		this.domainObjectMap = Maps.newHashMap();
 	}
 
 	@Override
 	protected void doRefactor() {
 		BoundedContext bc = ContextMappingDSLFactory.eINSTANCE.createBoundedContext();
 		bc.setName(newBoundedContextName);
+		Aggregate aggregate = ContextMappingDSLFactory.eINSTANCE.createAggregate();
+		aggregate.setName(newBoundedContextName + "Aggregate");
+		addElementToEList(bc.getAggregates(), aggregate);
 
-		// TODO: move nanoentities into new context
+		List<Attribute> allAttributesInSuggestedService = EcoreUtil2.eAllOfType(suggestedService, Attribute.class);
+		List<Reference> allReferencesInSuggestedService = EcoreUtil2.eAllOfType(suggestedService, Reference.class);
+		if (allAttributesInSuggestedService.isEmpty() && allReferencesInSuggestedService.isEmpty())
+			throw new ContextMapperApplicationException(
+					"This suggested service (Bounded Context) does not contain any Entities with data fields. We cannot extract such an empty Bounded Context. Please choose suggested Bounded Contexts that are not empty.");
+
+		for (Attribute serviceCutAttribute : allAttributesInSuggestedService) {
+			Attribute attribute = findAttributeInOriginalModel(serviceCutAttribute);
+			if (attribute != null)
+				moveAttribute(attribute, aggregate);
+		}
+		for (Reference serviceCutReference : allReferencesInSuggestedService) {
+			Reference reference = findReferenceInOriginalModel(serviceCutReference);
+			if (reference != null)
+				moveReference(reference, aggregate);
+		}
 
 		model.getBoundedContexts().add(bc);
+	}
+
+	private void moveAttribute(Attribute attribute, Aggregate newBCAggregate) {
+		DomainObject parentObject = (DomainObject) attribute.eContainer();
+
+		// create attribute in new domain object
+		DomainObject newDomainObject = getNewDomainObject(parentObject, newBCAggregate);
+		addElementToEList(newDomainObject.getAttributes(), EcoreUtil2.copy(attribute));
+
+		// remove attribute from original domain object
+		removeElementFromEList(parentObject.getAttributes(), attribute);
+
+		// remove original domain object if necessary
+		removeDomainObjectIfEmpty(parentObject);
+	}
+
+	private void moveReference(Reference reference, Aggregate newBCAggregate) {
+		DomainObject parentObject = (DomainObject) reference.eContainer();
+
+		// create attribute in new domain object
+		DomainObject newDomainObject = getNewDomainObject(parentObject, newBCAggregate);
+		addElementToEList(newDomainObject.getReferences(), EcoreUtil2.copy(reference));
+
+		// remove attribute from original domain object
+		removeElementFromEList(parentObject.getReferences(), reference);
+
+		// remove original domain object if necessary
+		removeDomainObjectIfEmpty(parentObject);
+	}
+
+	private Attribute findAttributeInOriginalModel(Attribute serviceCutAttribute) {
+		if (!(serviceCutAttribute.eContainer() instanceof DomainObject))
+			throw new ContextMapperApplicationException(
+					"We unexpectedly found attributes that are not contained by 'DomainObject' objects. We currently only support Entities, ValueObjects and Events here.");
+
+		String domainObjectName = ((DomainObject) serviceCutAttribute.eContainer()).getName();
+		String attributeName = serviceCutAttribute.getName();
+
+		for (Attribute attribute : EcoreUtil2.eAllOfType(model, Attribute.class).stream().filter(a -> a.getName().equals(attributeName)).collect(Collectors.toList())) {
+			if (attribute.eContainer() instanceof DomainObject && ((DomainObject) attribute.eContainer()).getName().equals(domainObjectName))
+				return attribute;
+		}
+
+		return null;
+	}
+
+	private Reference findReferenceInOriginalModel(Reference serviceCutReference) {
+		if (!(serviceCutReference.eContainer() instanceof DomainObject))
+			throw new ContextMapperApplicationException(
+					"We unexpectedly found references that are not contained by 'DomainObject' objects. We currently only support Entities, ValueObjects and Events here.");
+
+		String domainObjectName = ((DomainObject) serviceCutReference.eContainer()).getName();
+		String attributeName = serviceCutReference.getName();
+
+		for (Reference reference : EcoreUtil2.eAllOfType(model, Reference.class).stream().filter(r -> r.getName().equals(attributeName)).collect(Collectors.toList())) {
+			if (reference.eContainer() instanceof DomainObject && ((DomainObject) reference.eContainer()).getName().equals(domainObjectName))
+				return reference;
+		}
+
+		return null;
+	}
+
+	private DomainObject getNewDomainObject(DomainObject originalDomainObject, Aggregate newBCAggregate) {
+		if (this.domainObjectMap.containsKey(originalDomainObject.getName()))
+			return this.domainObjectMap.get(originalDomainObject.getName());
+
+		DomainObject domainObject = TacticdslFactory.eINSTANCE.createEntity();
+		if (originalDomainObject instanceof ValueObject)
+			domainObject = TacticdslFactory.eINSTANCE.createValueObject();
+		if (originalDomainObject instanceof DomainEvent)
+			domainObject = TacticdslFactory.eINSTANCE.createDomainEvent();
+		domainObject.setName(originalDomainObject.getName());
+		addElementToEList(newBCAggregate.getDomainObjects(), domainObject);
+		this.domainObjectMap.put(originalDomainObject.getName(), domainObject);
+		return domainObject;
+	}
+
+	private void removeDomainObjectIfEmpty(DomainObject domainObject) {
+		if (!domainObject.getAttributes().isEmpty())
+			return;
+		if (!domainObject.getReferences().isEmpty())
+			return;
+
+		if (domainObject.eContainer() instanceof Aggregate)
+			removeElementFromEList(((Aggregate) domainObject.eContainer()).getDomainObjects(), domainObject);
+		else if (domainObject.eContainer() instanceof SculptorModule)
+			removeElementFromEList(((SculptorModule) domainObject.eContainer()).getDomainObjects(), domainObject);
 	}
 
 	public URI constructOriginalModelUri() {
