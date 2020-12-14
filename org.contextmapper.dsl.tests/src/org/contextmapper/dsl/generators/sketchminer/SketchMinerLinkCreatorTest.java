e57049d5b8603d42ab62e5d8f28fe0c67ce52425
Stefan Kapferer <stefan@kapferer.ch>
2020-12-14 19:58:58 +0100

    Testing improvement

--- org.contextmapper.dsl.tests/src/org/contextmapper/dsl/generators/sketchminer/SketchMinerLinkCreatorTest.java
+++ org.contextmapper.dsl.tests/src/org/contextmapper/dsl/generators/sketchminer/SketchMinerLinkCreatorTest.java
@@ -20,9 +20,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 
 import org.contextmapper.dsl.AbstractCMLInputFileTest;
+import org.contextmapper.dsl.contextMappingDSL.ContextMappingDSLFactory;
 import org.contextmapper.dsl.contextMappingDSL.ContextMappingModel;
 import org.contextmapper.dsl.contextMappingDSL.Flow;
 import org.contextmapper.dsl.generator.sketchminer.SketchMinerLinkCreator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
 import org.junit.jupiter.api.Test;
 
@@ -41,6 +43,33 @@ public class SketchMinerLinkCreatorTest extends AbstractCMLInputFileTest {
 		assertEquals(
 				"https://www.bpmn-sketch-miner.ai/index.html#EYBwNgdgXAbgjAKALRIQSQgZwK4CcCGEAxgKYCK2A9gC4mYAEAgiOAJZH7WuXQIID0-ehRokASiQCO2OtXoBtekgB89AMoBVAEIBZNABV9AUQAiAXQSYSuGOxLrswALatqE6bITuZmamscu1LQAJgJCIrTesgqaugbGJkqqYkYAwkZoAGqm9AAa9CkAUmkJFlY2dvREABYkRADWUb4IEeJ1JKwwJKGCwlSRUj5y8inpWTkqBUbFqQl5TKnpAApz+Ua5S2gp5pbWtqT0uO2dJIwQwam1Da0t-adEpCAhYX2iTcOMi0YrE6pLAPIAGTQqQAmgB9VIpRirejrTbbMp7Sr4B4kJ63URGAAeIFYR1CL1a7wUSXU2j0hlMSIqB38zlc7y8g1k9MCz16xJZvhiFPivymY2yiXyRRK1N2tPsNTqjW51ExA1IJx64TuJJGaQywrJYtmOXyn2WsPhWwl5X29iOyq6ZwuV3qN1ajDRT26RPV8oURu+c0mAOBYMh0JNGzNOwtKNdCqWlDA7AAnqkjpx3R63l7FJNYpTSpLLQ4GW55cyPL42a4OWqM2Xhjn+YlJqNtQapjM85GDjKGkyuTb3ZzPbWFM3xo3ktNxSKFsbW6bEfnKtaOrbzpdZU7PQArOrPdMDYdZ1T1qkR5F0gKMkvvCtBAfVg9DXlxU+6rVj+Z6jvn6UO3ue-tVVeR9ok1IUBS-VsfR+ad53NH9DmOVd7Q3O5FRIHE8QJPhBxrJ8j3JF9vylQtAiZG9LzvICuUPE8-QncDp0gs8SO7OVa1LIYJB3Ig9yAA",
 				link);
+	}
+
+	@Test
+	public void canCreateSketchMinerLinkWithEObject() throws IOException {
+		// given
+		ContextMappingModel model = getOriginalResourceOfTestCML("link-integ-test.cml").getContextMappingModel();
+		EObject flow = EcoreUtil2.eAllOfType(model, Flow.class).get(0);
+
+		// when
+		String link = new SketchMinerLinkCreator().createSketchMinerLink(flow);
+
+		// then
+		assertEquals(
+				"https://www.bpmn-sketch-miner.ai/index.html#EYBwNgdgXAbgjAKALRIQSQgZwK4CcCGEAxgKYCK2A9gC4mYAEAgiOAJZH7WuXQIID0-ehRokASiQCO2OtXoBtekgB89AMoBVAEIBZNABV9AUQAiAXQSYSuGOxLrswALatqE6bITuZmamscu1LQAJgJCIrTesgqaugbGJkqqYkYAwkZoAGqm9AAa9CkAUmkJFlY2dvREABYkRADWUb4IEeJ1JKwwJKGCwlSRUj5y8inpWTkqBUbFqQl5TKnpAApz+Ua5S2gp5pbWtqT0uO2dJIwQwam1Da0t-adEpCAhYX2iTcOMi0YrE6pLAPIAGTQqQAmgB9VIpRirejrTbbMp7Sr4B4kJ63URGAAeIFYR1CL1a7wUSXU2j0hlMSIqB38zlc7y8g1k9MCz16xJZvhiFPivymY2yiXyRRK1N2tPsNTqjW51ExA1IJx64TuJJGaQywrJYtmOXyn2WsPhWwl5X29iOyq6ZwuV3qN1ajDRT26RPV8oURu+c0mAOBYMh0JNGzNOwtKNdCqWlDA7AAnqkjpx3R63l7FJNYpTSpLLQ4GW55cyPL42a4OWqM2Xhjn+YlJqNtQapjM85GDjKGkyuTb3ZzPbWFM3xo3ktNxSKFsbW6bEfnKtaOrbzpdZU7PQArOrPdMDYdZ1T1qkR5F0gKMkvvCtBAfVg9DXlxU+6rVj+Z6jvn6UO3ue-tVVeR9ok1IUBS-VsfR+ad53NH9DmOVd7Q3O5FRIHE8QJPhBxrJ8j3JF9vylQtAiZG9LzvICuUPE8-QncDp0gs8SO7OVa1LIYJB3Ig9yAA",
+				link);
+	}
+
+	@Test
+	public void canReturnEmptyString4WrongObject() {
+		// given
+		EObject obj = ContextMappingDSLFactory.eINSTANCE.createBoundedContext();
+
+		// when
+		String link = new SketchMinerLinkCreator().createSketchMinerLink(obj);
+
+		// then
+		assertEquals("", link);
 	}
 
 	@Override
